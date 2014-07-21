a0ef3a5c9a1f7955b0000b3036801604fcf60a5b
Dinko Ferencek <Dinko.Ferencek@cern.ch>
2014-07-21 15:17:13 -0500

    more generic implementation of NjettinessAdder

--- RecoJets/JetProducers/python/nJettinessAdder_cfi.py
+++ RecoJets/JetProducers/python/nJettinessAdder_cfi.py
@@ -2,5 +2,6 @@ import FWCore.ParameterSet.Config as cms
 
 Njettiness = cms.EDProducer("NjettinessAdder",
                             src=cms.InputTag("ak5PFJetsCHS"),
-                            cone=cms.double(0.5)
+                            cone=cms.double(0.5),
+                            Njets=cms.vuint32(1,2,3)
                             )

8ccf242f857730dca3a3052b4fa53176c6f75b88
Dinko Ferencek <Dinko.Ferencek@cern.ch>
2014-11-27 01:29:07 -0600

    CSVv2+IVF tagger now has the same name as in CMSSW>=7_2_0

--- PhysicsTools/PatAlgos/python/tools/jetTools.py
+++ PhysicsTools/PatAlgos/python/tools/jetTools.py
@@ -46,7 +46,7 @@ _defaultBTagDiscriminators =['jetBProbabilityBJetTags'
                             #,'combinedSecondaryVertexV1BJetTags'
                             #,'combinedSecondaryVertexV1PositiveBJetTags'
                             #,'combinedSecondaryVertexMVABJetTags'
-                            #,'combinedSecondaryVertexV2BJetTags'
+                            #,'combinedInclusiveSecondaryVertexV2BJetTags'
                             #,'combinedSecondaryVertexNegativeBJetTags'
                             #,'combinedSecondaryVertexV1NegativeBJetTags'
                             #,'softPFMuonBJetTags'
@@ -104,7 +104,7 @@ _allowedBTagDiscriminators =['jetBProbabilityBJetTags'
                             ,'combinedSecondaryVertexV1BJetTags'
                             ,'combinedSecondaryVertexV1PositiveBJetTags'
                             ,'combinedSecondaryVertexMVABJetTags'
-                            ,'combinedSecondaryVertexV2BJetTags'
+                            ,'combinedInclusiveSecondaryVertexV2BJetTags'
                             ,'combinedSecondaryVertexNegativeBJetTags'
                             ,'combinedSecondaryVertexV1NegativeBJetTags'
                             ,'softPFMuonBJetTags'
@@ -413,7 +413,7 @@ class RunBTagging(ConfigToolBase):
                     else:
                         print '  --> ignored, since input %s not available!'%( vit(ivfFiltTILabel) )
 
-                if tag == 'combinedInclusiveSecondaryVertexBJetTags' or tag == 'combinedInclusiveSecondaryVertexPositiveBJetTags' or tag == 'combinedSecondaryVertexV2BJetTags':
+                if tag == 'combinedInclusiveSecondaryVertexBJetTags' or tag == 'combinedInclusiveSecondaryVertexPositiveBJetTags' or tag == 'combinedInclusiveSecondaryVertexV2BJetTags':
                     if hasattr( process, ipTILabel ) and hasattr( process, ivfTILabel ):
                         if not hasattr(process, tag+label+postfix):
                             tagMod=getattr(btag,tag).clone(tagInfos = vit(ipTILabel,ivfTILabel))
